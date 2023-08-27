e11473e75509b6a57ca3800a9252dbff28e5a035
Mikerophonic <micgcon@gmail.com>
2023-08-26 18:46:07 -0700

    add _side3 get functionality + passing test

--- ModelTests/TriangleTests.cs
+++ ModelTests/TriangleTests.cs
@@ -10,7 +10,7 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle(2, 3); // Updated!
+      Triangle newTriangle = new Triangle(2, 3, 8); // Updated!
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
 
@@ -18,7 +18,7 @@ namespace ShapeTracker.Tests
     public void GetSide1_ReturnsSide1_Int()
     {
       int length1 = 3;
-      Triangle newTriangle = new Triangle(length1, 2);
+      Triangle newTriangle = new Triangle(length1, 2, 8);
       int result = newTriangle.Side1;
       Assert.AreEqual(length1, result);
     }
@@ -27,7 +27,7 @@ namespace ShapeTracker.Tests
     public void SetSide1_SetsValueOfSide1_Void()
     {
       // Arrange
-      Triangle newTriangle = new Triangle(3, 3);
+      Triangle newTriangle = new Triangle(3, 3, 8);
       int newLength1 = 44;
       // Act
       newTriangle.Side1 = newLength1;
@@ -40,7 +40,7 @@ namespace ShapeTracker.Tests
     {
       // Arrange
       int length2 = 3;
-      Triangle newTriangle = new Triangle(2, length2);
+      Triangle newTriangle = new Triangle(2, length2, 8);
       // Act
       int result = newTriangle.Side2;
       // Assert
@@ -51,7 +51,7 @@ namespace ShapeTracker.Tests
     public void SetSide2_SetsValueOfSide2_Void()
     {
       // Arrange
-      Triangle newTriangle = new Triangle(3,4);
+      Triangle newTriangle = new Triangle(3, 4, 8);
       int newLength2 = 6;
       // Act
       newTriangle.Side2 = newLength2;
@@ -59,6 +59,17 @@ namespace ShapeTracker.Tests
       Assert.AreEqual(newLength2, newTriangle.Side2);
     }
 
+    [TestMethod]
+    public void GetSide3_ReturnsSide3_Int()
+    {
+      // Arrange
+      int length3 = 55;
+      Triangle newTriangle = new Triangle(2, 3, length3);
+      // Act
+      int result = newTriangle.GetSide3();
+      // Assert
+      Assert.AreEqual(length3, result);
+    }
 
   }
 }
