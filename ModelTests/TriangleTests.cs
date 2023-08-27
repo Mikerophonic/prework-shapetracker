87e21aa977cb091fbe2956bf82d4998469a3c8b0
Mikerophonic <micgcon@gmail.com>
2023-08-26 18:22:30 -0700

    first commit

---
+++ ModelTests/TriangleTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeTracker.Models;
+
+namespace ShapeTracker.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  {
+
+    [TestMethod]
+    public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
+    {
+      Triangle newTriangle = new Triangle();
+      Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+    }
+
+  }
+}
