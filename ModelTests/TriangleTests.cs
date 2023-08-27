3f053f9132b608b51af26addbdcfd226fa2794ec
Mikerophonic <micgcon@gmail.com>
2023-08-26 18:47:18 -0700

    add _side3 set functionality + passing test

--- ModelTests/TriangleTests.cs
+++ ModelTests/TriangleTests.cs
@@ -71,5 +71,17 @@ namespace ShapeTracker.Tests
       Assert.AreEqual(length3, result);
     }
 
+    [TestMethod]
+    public void SetSide3_SetsValueOfSide3_Void()
+    {
+      // Arrange
+      Triangle newTriangle = new Triangle(3, 4, 8);
+      int newLength3 = 6;
+      // Act
+      newTriangle.SetSide3(newLength3);
+      // Assert
+      Assert.AreEqual(newLength3, newTriangle.GetSide3());
+    }
+    
   }
 }
