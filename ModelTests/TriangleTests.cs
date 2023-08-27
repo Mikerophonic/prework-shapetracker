7b6c69dff52e7eb659c2a2ce6c7931a8cdfcf274
Mikerophonic <micgcon@gmail.com>
2023-08-26 18:31:40 -0700

    add Side1 get functionality + passing test.

--- ModelTests/TriangleTests.cs
+++ ModelTests/TriangleTests.cs
@@ -10,8 +10,17 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle();
+      Triangle newTriangle = new Triangle(2); // Updated!
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+    }
+
+    [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    {
+      int length1 = 3;
+      Triangle newTriangle = new Triangle(length1);
+      int result = newTriangle.Side1;
+      Assert.AreEqual(length1, result);
     }
 
   }
